7dc988ab5463ab5af235ea57335c312ec72860e7
Emo <eiv.razgrad@gmail.com>
2020-08-01 17:27:27 +0300

    feat: try implement search

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,24 +4,27 @@ import Product from "../../components/product";
 import PageWrapper from "../../components/page-wrapper";
 import Title from "../../components/title";
 import Products from "../../components/products";
-import UserContext from '../../Context'
+import Input from "../../components/Input";
+import UserContext from "../../Context";
 
 class Publications extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      products: []
+      products: [],
+      result: [],
+      title: "",
     };
   }
-  
-  static contextType = UserContext
+
+  static contextType = UserContext;
 
   getProducts = async () => {
     const promise = await fetch("http://localhost:9999/api/product");
     const products = await promise.json();
     this.setState({
-      products
+      products,
     });
   };
 
@@ -33,17 +36,61 @@ class Publications extends Component {
     });
   }
 
+  renderSearch() {
+ 
+    const { result } = this.state;
+
+    return result.map((product, index) => {
+      return <Product key={product._id} index={index} {...product} />;
+    });
+  }
+
   componentDidMount() {
     this.getProducts();
   }
+  handleChange = (event, type) => {
+    const newState = {};
+    newState[type] = event.target.value;
+    this.setState(newState);
+  };
+
+  Search=()=>{
+    debugger
+    const { title, products } = this.state;
+
+    const searchResult = products.filter(find => find.title === title)
+
+    this.setState({
+      result: searchResult
+    })
+    debugger
+    console.log("Here SearchResult=>", searchResult)
+
+
+  }
 
   render() {
+    const { title } = this.state;
+
 
     return (
       <PageWrapper>
         <Title title="Home" />
+   
+        <form  onSubmit={this.Search}>
+ 
+          <Input
+          value={title}
+          onChange={(e) => this.handleChange(e, "title")}
+          label="Title"
+          id="title"
+        />
+        <button >Search</button>
+        </form>
+ 
 
-        <Products/>
+        
+        <Products />
       </PageWrapper>
     );
   }
