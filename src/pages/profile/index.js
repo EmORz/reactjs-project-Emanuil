9e60cf07a6f29c1ba82d259d30fe939c80191c13
Emo <eiv.razgrad@gmail.com>
2020-08-01 13:17:46 +0300

    feat: profile user posts

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,53 +1,51 @@
 import React, { Component } from "react";
 import PageWrapper from "../../components/page-wrapper";
-import UserContext from '../../Context'
-import Button from '../../components/button/submit-button'
-import style from './index.module.css'
-
+import UserContext from "../../Context";
+import Button from "../../components/button/submit-button";
+import style from "./index.module.css";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
       username: null,
-      posts: null,
+      products: [],
     };
   }
 
-  static contextType = UserContext
+  static contextType = UserContext;
 
-  logOut = ()=>{
- 
-    this.context.logOut()
-    this.props.history.push('/')
-  }
+  logOut = () => {
+    this.context.logOut();
+    this.props.history.push("/");
+  };
   componentDidMount() {
     this.getUser(this.props.match.params.userid);
   }
 
   getUser = async (userid) => {
-      
     const promisse = await fetch(`http://localhost:9999/api/user?id=${userid}`);
 
-   const user = await promisse.json();
-
+    const promise = await fetch("http://localhost:9999/api/product");
+    const products = await promise.json();
+    
+    const user = await promisse.json();
     this.setState({
       username: user.username,
-      posts: user.posts && user.posts.length,
+      products: products.filter((p) => p.author === user._id),
     });
   };
   render() {
-    const { username, posts } = this.state;
+    const { username, products } = this.state;
+
     return (
       <PageWrapper>
         <form className={style.container} onSubmit={this.logOut}>
           <p>User: {username}</p>
-          <p>Posts: {posts}</p>
+          <p>Created Posts/Products: {products.length} pieces</p>
 
-          <Button  title="Logout" />
-   
+          <Button title="Logout" />
         </form>
- 
       </PageWrapper>
     );
   }
